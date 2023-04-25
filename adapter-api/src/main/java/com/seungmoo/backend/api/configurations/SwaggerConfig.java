a6621d1ca5be354a92175ff730497e18ba32c631
dltmdan92 <dltmdan92@naver.com>
2023-04-25 19:22:25 +0900

    스웨거 추가

---
+++ adapter-api/src/main/java/com/seungmoo/backend/api/configurations/SwaggerConfig.java
@@ -0,0 +1,17 @@
+package com.seungmoo.backend.api.configurations;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI springShopOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("backend-seungmoo API")
+                        .description("backend-seungmoo API"));
+    }
+}
