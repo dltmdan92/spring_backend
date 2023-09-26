111617a9fc0d0881ba4bbae5c014ca7ac498e2e1
dltmdan92 <dltmdan92@naver.com>
2023-09-26 19:46:11 +0900

    수정

--- adapter-api/src/main/java/com/seungmoo/backend/api/configurations/WebConfig.java
+++ adapter-api/src/main/java/com/seungmoo/backend/api/configurations/WebConfig.java
@@ -6,13 +6,18 @@ import com.seungmoo.backend.api.service.user.providers.SessionProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.reactive.function.server.*;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import reactor.core.publisher.Mono;
 
 import java.util.List;
+
+import static org.springframework.web.reactive.function.server.RequestPredicates.accept;
 
 @Configuration
 @RequiredArgsConstructor
@@ -22,6 +27,8 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
+        RouterFunctions.route(RequestPredicates.POST("/users").and(accept(MediaType.APPLICATION_JSON)), request -> ServerResponse.ok().build());
+
         registry.addInterceptor(new SessionUserInterceptor(sessionProvider))
                 .addPathPatterns("/**")
                 .excludePathPatterns("/swagger-ui/**");
